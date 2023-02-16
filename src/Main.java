4a86e60fed6489c5faca1342f6ae653ca060c8c1
[RoomanHass] <[hasan0v.rom@yandex.ru]>
2023-02-16 17:24:23 +0300

    Внесение мелких правок

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
         MagicBox<String> boxString = new MagicBox<>(5);
         MagicBox<Integer> boxInteger = new MagicBox<>(7);
 
-        boxString.add("запись 23");
-        boxString.add("Значение 1");
-        boxString.add("Книга");
-        boxString.add("Сборник книг");
-        boxString.add("Строка из книги");
+        boxString.add("Удача");
+        boxString.add("Везение");
+        boxString.add("Сила");
+        boxString.add("Счастье");
+        boxString.add("Стремление");
         String a = boxString.pick();
         System.out.println(a);
 
-        boxInteger.add(3);
+        boxInteger.add(333);
         boxInteger.add(12);
         boxInteger.add(42);
-        boxInteger.add(6);
+        boxInteger.add(600);
         boxInteger.add(58);
         boxInteger.add(6);
         boxInteger.add(38);
